99426f07d478ccbcd63385a7cc433c6bf57d5f01
Ivan Logvinov <ivan.logvinov.10@gmail.com>
2025-12-23 22:47:41 +0300

    Add them params page

--- src/navigation/routes.tsx
+++ src/navigation/routes.tsx
@@ -1,8 +1,9 @@
-import type { ComponentType, JSX } from 'react';
+import type { ComponentType, JSX } from "react";
 
 // import { IndexPage } from '@/pages/IndexPage/IndexPage';
-import { AddWorkoutPage } from '@/pages/AddWorkoutPage';
-import { MainPage } from '@/pages/MainPage/MainPage';
+import { AddWorkoutPage } from "@/pages/AddWorkoutPage";
+import { MainPage } from "@/pages/MainPage/MainPage";
+import { ThemeParamsPage } from "@/pages/ThemeParamsPage";
 
 interface Route {
   path: string;
@@ -12,6 +13,7 @@ interface Route {
 }
 
 export const routes: Route[] = [
-  { path: '/', Component: MainPage },
-  {path: '/workouts/new', Component: AddWorkoutPage, title: 'Add Workout'}
+  { path: "/", Component: MainPage },
+  { path: "/theme-params", Component: ThemeParamsPage, title: "Theme Params" },
+  { path: "/workouts/new", Component: AddWorkoutPage, title: "Add Workout" },
 ];
