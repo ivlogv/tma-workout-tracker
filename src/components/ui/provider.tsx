ec9cb4fe538fafc1f1c808d6f640145a0da336b5
Ivan <69221768+ivlogv@users.noreply.github.com>
2025-12-24 15:32:15 +0300

    Update provider.tsx

--- src/components/ui/provider.tsx
+++ src/components/ui/provider.tsx
@@ -1,25 +1,30 @@
 "use client";
 
 import { ChakraProvider } from "@chakra-ui/react";
-import { ColorModeProvider, type ColorModeProviderProps } from "./color-mode";
+// import { ColorModeProvider, type ColorModeProviderProps } from "./color-mode";
 import { system } from "@/components/theme";
 import { miniApp, useSignal, themeParams } from "@tma.js/sdk-react";
 // import { RegisterProvider } from "./RegisterProvider";
 // import { WorkoutProvider } from "./WorkoutProvider";
 
-export function Provider(props: ColorModeProviderProps) {
+type Props = {
+  children: React.ReactNode;
+}
+
+export function Provider(children: Props) {
   const tp = themeParams.state();
   console.log(tp.children);
   const isDark = useSignal(miniApp.isDark);
   return (
     <ChakraProvider value={system}>
-      <ColorModeProvider {...props} forcedTheme={isDark ? "dark" : "light"}>
+      {children}
+      {/* <ColorModeProvider {...props} forcedTheme={isDark ? "dark" : "light"}> */}
         {/* <RegisterProvider> */}
         {/* <WorkoutProvider> */}
         {/* {props.children} */}
         {/* </WorkoutProvider> */}
         {/* </RegisterProvider> */}
-      </ColorModeProvider>
+       {/* </ColorModeProvider> */}
     </ChakraProvider>
   );
 }
